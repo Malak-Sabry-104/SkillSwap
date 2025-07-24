20435ab605672ec04786873e7667c38ea63ee83e
Malak Sabry <malaksabry367@gmail.com>
2025-07-24 10:01:14 +0300

    up

--- src/Components/Layout.tsx
+++ src/Components/Layout.tsx
@@ -4,10 +4,13 @@ import NavBar from "./NavBar";
 import { Outlet } from "react-router-dom";
 
 const Layout = () => {
+  useEffect(() => {
+    document.title = "SkillSwapy | Share Skills, Learn Together";
+  }, []);
   return (
     <>
       <NavBar />
-        <ToastContainer
+      <ToastContainer
         position="top-right"
         autoClose={3000}
         theme="dark"
@@ -15,9 +18,12 @@ const Layout = () => {
         hideProgressBar={false}
       />
       <Outlet />
-      <Footer/>
+      <Footer />
     </>
   );
 };
 
 export default Layout;
+function useEffect(arg0: () => void, arg1: never[]) {
+  throw new Error("Function not implemented.");
+}
