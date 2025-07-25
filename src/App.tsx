d382af78d22a4092602b0968a574bd0e800cc41c
Khaled Muhammad <khaledmuhmmed99@gmail.com>
2025-07-25 21:45:08 +0300

    added supabaseProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import FeaturedSkills from "./Components/FeaturedSkills";
 import Hero from "./Components/Hero";
 import HowItWorks from "./Components/HowItWorks";
+import RequestsPage from './Routes/Requests';
+import { SupabaseProvider } from './Components/SkillContext';
+import { Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <>
+    <SupabaseProvider>
       <Hero />
       <FeaturedSkills />
       <HowItWorks/>
-    </>
+    </SupabaseProvider>
   );
 };
 
